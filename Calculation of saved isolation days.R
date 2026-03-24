a939e3111262c57cf63c938f6efee12dedc34c0f
Rune Christensen <runech1999@gmail.com>
2026-03-24 11:51:37 +0100

    Refactor saved isolation days calculations

--- Calculation of saved isolation days.R
+++ Calculation of saved isolation days.R
@@ -20,8 +20,30 @@ boot_res$t0
 # 95% CI
 boot.ci(boot_res, type = "perc")
 #----------------------------------------------------------------------------------------
-library(boot)
+# number of saved isolation days for Culture
+saved_prop_fun_neg <- function(data, indices) {
+  d <- data[indices, ]
+  
+  # number of saved isolation days
+  sum(d$`Dage i isolation`[d$CT_value > 31], na.rm = TRUE) /
+    sum(d$`Dage i isolation`, na.rm = TRUE)
+}
 
+set.seed(42)
+
+boot_res_prop_neg <- boot(
+  data = Datafile,
+  statistic = saved_prop_fun_neg,
+  R = 10000
+)
+
+# estimated proportion of saved isolation days
+boot_res_prop_neg$t0
+
+# 95% CI
+boot.ci(boot_res_prop_neg, type = "perc")
+
+#----------------------------------------------------------------------------------------
 # number of saved isolation days for Culture == "Negative"
 saved_prop_fun_neg <- function(data, indices) {
   d <- data[indices, ]
